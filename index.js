4b223537c0ad4009bf61b5d609c1d0bc10cb6a2e
unknown <suhasmaverick@gmail.com>
2024-09-26 21:20:49 +0530

    limit 5 req per min

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = process.env.PORT
 
 // Apply rate limiting to all requests
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    max: 5, // limit each IP to 100 requests per windowMs
     message: "Too many requests from this IP, please try again later."
 });
 
