1a380184c4a7fdc3d14a6129c99103d7a943e955
unknown <suhasmaverick@gmail.com>
2024-09-26 21:10:54 +0530

    Added rate-limiting middleware

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./model/product.model.js')
 const productRoute = require('./routes/product.route.js')
+const rateLimit = require('express-rate-limit');
 const app = express()
 const port = process.env.PORT
+
+
+// Apply rate limiting to all requests
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again later."
+});
 
 // ** EXPRESS MIDDLEWARE
 app.use(express.json())
@@ -18,7 +27,7 @@ app.get('/', (req, res) => {
     res.send('Hello World Updated LTS')
 })
 
-mongoose.connect('mongodb+srv://suhasmaverick:1LKJxXh84xYs6e15@pms.drciu.mongodb.net/?retryWrites=true&w=majority&appName=pms')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('Connected!')
         app.listen(port, () => {
